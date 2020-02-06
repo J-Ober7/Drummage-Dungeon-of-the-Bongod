a1ed4092b47061f4a5ef78c7ff18df1ae9b7a90d
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-02-05 22:14:57 -0500

    Notes Draft

---
+++ Assets/Scripts/Beat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat {
+
+    public enum Note {A, B, C, D, NONE};
+    private Note n1;
+    private Note n2;
+    
+    public Beat() {
+        n1 = Note.NONE;
+        n2 = Note.NONE;
+    }
+
+    public void addNote() {
+
+    }
+    
+}
