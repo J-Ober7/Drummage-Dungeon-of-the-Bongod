471497483c506470b092141b3b7380541e8a1d14
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-02-08 11:41:21 -0500

    Battle Input Works

--- Assets/Scripts/Beat.cs
+++ Assets/Scripts/Beat.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Beat {
-
+    
     public enum Note {A, B, C, D, NONE};
     private Note n1;
     private Note n2;
@@ -13,8 +13,64 @@ public class Beat {
         n2 = Note.NONE;
     }
 
-    public void addNote() {
+    public Beat(Note na, Note nb) {
+        n1 = na;
+        n2 = nb;
+    }
 
+    public void addNote(Note n) {
+        if(n1 == Note.NONE) {
+            n1 = n;
+        }else if (n2 == Note.NONE) {
+            n2 = n;
+        }
+    }
+
+    public string toString() {
+
+        string s = "";
+        switch (n1) {
+            case Note.NONE:
+                s += "-";
+                break;
+            case Note.A:
+                s += "A";
+                break;
+            case Note.B:
+                s += "B";
+                break;
+            case Note.C:
+                s += "C";
+                break;
+            case Note.D:
+                s += "D";
+                break;
+            default:
+                break;
+
+        }
+        switch (n2) {
+            case Note.NONE:
+                s += "-";
+                break;
+            case Note.A:
+                s += "A";
+                break;
+            case Note.B:
+                s += "B";
+                break;
+            case Note.C:
+                s += "C";
+                break;
+            case Note.D:
+                s += "D";
+                break;
+            default:
+                break;
+
+        }
+
+        return s;
     }
     
 }
---
+++ Assets/Scripts/Pattern.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pattern : ScriptableObject
+{
+
+    public Beat[] beats;
+    // Start is called before the first frame update
+    public Pattern() {
+        beats = new Beat[] { new Beat(), new Beat(), new Beat(), new Beat() };
+    }
+
+    public Pattern(Beat b1, Beat b2, Beat b3, Beat b4) {
+        beats = new Beat[] { b1, b2, b3, b4 };
+    }
+
+    // Update is called once per frame
+    public void addNote(Beat.Note note, int count) {
+        
+        beats[count].addNote(note);
+
+
+    }
+
+    public string toString() {
+        string s = "";
+        s += beats[0].toString();
+        s += " ";
+        s += beats[1].toString();
+        s += " ";
+        s += beats[2].toString();
+        s += " ";
+        s += beats[3].toString();
+        s += "|";
+
+        return s;
+    }
+}
