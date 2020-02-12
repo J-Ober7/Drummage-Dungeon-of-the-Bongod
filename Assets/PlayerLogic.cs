0fa73ffae1e6191a7dd24f2f00373cdc5f72e367
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-02-12 18:50:34 -0500

    BrokeTransition

---
+++ Assets/PlayerLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.CompareTag("Enemy")) {
+            SceneManager.LoadScene("BattleTestScene");
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+
+        Debug.Log("Entering0");
+        if (other.CompareTag("Enemy")) {
+            SceneManager.LoadScene("BattleTestScene");
+        }
+    }
+}
