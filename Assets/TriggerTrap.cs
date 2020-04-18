2f8eff6d69a99bab7978acdb3e72aac88cb8c169
josebechara97 <josebechara97@hotmail.com>
2020-04-18 18:10:34 -0400

    TriggerTrap
    -TriggerTrap Scene
    -Explosion Prefabs
    -Explosion Sound
    -TriggerTrap Prefab with TriggerTrap script

---
+++ Assets/TriggerTrap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTrap : MonoBehaviour
+{
+    public GameObject explosion;
+    public int damage = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            other.gameObject.GetComponent<PlayerBattle>().takeDamage(damage);
+        }
+       
+    }
+}
